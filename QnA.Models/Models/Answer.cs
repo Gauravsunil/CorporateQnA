106c78b15ac4991052d51627075461ccc21e0d67
Gauravsunil <32197840+Gauravsunil@users.noreply.github.com>
2021-02-02 16:03:57 +0530

    Add files via upload

---
+++ QnA.Models/Models/Answer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QnA.Models.Models
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public List<string> Likes { get; set; }
+        public List<string> Dislikes { get; set; }
+        public int QuestionId { get; set; }
+        public string UserId { get; set; }
+        public bool IsBestSolution { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Answer()
+        {
+            this.CreatedOn = DateTime.Now;
+        }
+    }
+
+    
+}
---
+++ QnA.Models/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QnA.Models.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
